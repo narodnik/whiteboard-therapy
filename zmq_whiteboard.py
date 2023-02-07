9c844a3ebdfd6d02cc2b8f2ebcfc4c4a3c6ac025
parazyd <parazyd@dyne.org>
2023-02-07 01:51:20 +0100

    poc

--- zmq_whiteboard.py
+++ zmq_whiteboard.py
@@ -1,66 +1,179 @@
 #!/usr/bin/env python3
 import asyncio
 import time
-from queue import Queue
+from argparse import ArgumentParser
 
 import pygame
 import zmq
 import zmq.asyncio
 
-TOPIC = "Therapy"
-Q = asyncio.Queue(maxsize=0)
-CTX = zmq.asyncio.Context()
-
-
-class Sub:
-    def __init__(self, topic, frontend):
-        self.topic = topic
+class Appointment:
+    def __init__(self, name, topic, frontend, backend):
         self.frontend = frontend
-        self.loop = True
+        self.backend = backend
+        self.ctx = zmq.asyncio.Context()
+
+        self.topic = topic
+        self.name = name
+
+        self.pub = self.ctx.socket(zmq.PUB)
+        self.pub.connect(self.backend)
+
+        self.sub = self.ctx.socket(zmq.SUB)
+        self.sub.connect(self.frontend)
+        self.sub.setsockopt_string(zmq.SUBSCRIBE, self.topic)
+
+        self.stopped = False
+
+        self.patients = {}
+
+        self.screen = pygame.display.set_mode((800, 600), pygame.RESIZABLE)
+        self.wacom_x = 0
+        self.wacom_y = 0
+        self.origin_x = 0
+        self.origin_y = 0
+        self.down = False
+        self.mouse_track = []
 
     async def subscribe(self):
         print("Start subscriber")
-        subscriber = CTX.socket(zmq.SUB)
-        subscriber.connect(self.frontend)
-        subscriber.setsockopt_string(zmq.SUBSCRIBE, self.topic)
-        while self.loop:
-            msg = await subscriber.recv()
-            print(f"Sub got msg: {msg}")
-            await Q.put(msg)
 
-    def stop(self):
-        self.loop = False
-
-
-class Board:
-    def __init__(self, topic, backend):
-        self.wacom_x, self.wacom_y = 0, 0
-        self.origin_x, self.origin_y = 0, 0
-        self.stopped = False
-        self.pub = CTX.socket(zmq.PUB)
-        self.pub.connect(backend)
-        self.topic = topic
-        self.mouse_track = []
-
-    async def recv_queue(self):
         while not self.stopped:
-            msg = await Q.get()
-            print(f"Got msg from queue: {msg}")
+            msg = await self.sub.recv_string()
+            print(f"Sub got msg: {msg}")
+            sp = msg.split(":")
+            topic = sp[0]
+            name = sp[1]
+            event = sp[2]
 
-    async def publish(self):
-        print(f"Start publisher")
+            if name == self.name:
+                continue
+
+            patient_data = self.patients.get(name, {
+                                             "wacom_x": 0,
+                                             "wacom_y": 0,
+                                             "origin_x": 0,
+                                             "origin_y": 0,
+                                             "mouse_track": [],
+                                             "down": False,
+                                             })
+
+            if event == "MouseMotion":
+                x, y = sp[3], sp[4]
+                patient_data["wacom_x"] = int(x)
+                patient_data["wacom_y"] = int(y)
+                if patient_data["down"]:
+                    patient_data["mouse_track"][-1].append((int(x), int(y)))
+
+            elif event == "MouseButtonDown":
+                patient_data["down"] = True
+                patient_data["mouse_track"].append([])
+
+            elif event == "MouseButtonUp":
+                patient_data["down"] = False
+
+            elif event == "OriginY":
+                patient_data["origin_y"] += int(sp[3])
+
+            elif event == "OriginX":
+                patient_data["origin_x"] += int(sp[3])
+
+            for segment in patient_data["mouse_track"]:
+                if not segment:
+                    continue
+                start = segment[0]
+                for end in segment[1:]:
+                    start_x, start_y = start
+                    end_x, end_y = end
+
+                    start_x += patient_data["origin_x"]
+                    start_y += patient_data["origin_y"]
+                    end_x += patient_data["origin_x"]
+                    end_y += patient_data["origin_y"]
+
+                    start = (start_x, start_y)
+                    adj_end = (end_x, end_y)
+
+                    pygame.draw.line(self.screen, (255,0,0), start, adj_end, width=1)
+                    start = end
+
+            pygame.draw.circle(self.screen, (255,255,255),
+                               (patient_data["wacom_x"], patient_data["wacom_y"]),
+                               8)
+            pygame.display.flip()
+
+            self.patients[name] = patient_data
 
     async def draw(self):
-        screen = pygame.display.set_mode((800, 600), pygame.RESIZABLE)
+        keydown_up = False
+        keydown_down = False
+        keydown_right = False
+        keydown_left = False
+
         last_time = time.time()
 
         while not self.stopped:
             for event in pygame.event.get():
-                if event.type == pygame.MOUSEMOTION:
-                    print("motion")
-                    self.pub.send_string(f"{self.topic}:motion")
+                if event.type == pygame.QUIT:
+                    print("Received quit!")
+                    self.stopped = True
+                    break
+                elif event.type == pygame.MOUSEMOTION:
+                    print("MouseMotion")
+                    self.wacom_x, self.wacom_y = pygame.mouse.get_pos()
+                    msg = f"{self.topic}:{self.name}:MouseMotion:{self.wacom_x}:{self.wacom_y}"
+                    await self.pub.send_string(msg)
+                    if self.down:
+                        self.mouse_track[-1].append(event.pos)
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    print("MouseButtonDown")
+                    self.down = True
+                    self.mouse_track.append([])
+                    msg = f"{self.topic}:{self.name}:MouseButtonDown"
+                    await self.pub.send_string(msg)
+                elif event.type == pygame.MOUSEBUTTONUP:
+                    print("MouseButtonUp")
+                    self.down = False
+                    msg = f"{self.topic}:{self.name}:MouseButtonUp"
+                    await self.pub.send_string(msg)
 
-            screen.fill(pygame.Color("black"))
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        keydown_up = True
+                    elif event.key == pygame.K_DOWN:
+                        keydown_down = True
+                    elif event.key == pygame.K_RIGHT:
+                        keydown_right = True
+                    elif event.key == pygame.K_LEFT:
+                        keydown_left = True
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_UP:
+                        keydown_up = False
+                    elif event.key == pygame.K_DOWN:
+                        keydown_down = False
+                    elif event.key == pygame.K_RIGHT:
+                        keydown_right = False
+                    elif event.key == pygame.K_LEFT:
+                        keydown_left = False
+
+            if keydown_up:
+                self.origin_y += 20
+                msg = f"{self.topic}:{self.name}:OriginY:{self.origin_y}"
+                await self.pub.send_string(msg)
+            if keydown_down:
+                self.origin_y -= 20
+                msg = f"{self.topic}:{self.name}:OriginY:{self.origin_y}"
+                await self.pub.send_string(msg)
+            if keydown_left:
+                self.origin_x += 20
+                msg = f"{self.topic}:{self.name}:OriginX:{self.origin_x}"
+                await self.pub.send_string(msg)
+            if keydown_right:
+                self.origin_x -= 20
+                msg = f"{self.topic}:{self.name}:OriginX:{self.origin_x}"
+                await self.pub.send_string(msg)
+
+            self.screen.fill(pygame.Color("black"))
 
             for segment in self.mouse_track:
                 if not segment:
@@ -78,34 +191,36 @@ class Board:
                     start = (start_x, start_y)
                     adj_end = (end_x, end_y)
 
-                    pygame.draw.line(screen, (255, 0, 0), start, adj_end,
-                                     width=1)
-
+                    pygame.draw.line(self.screen, (255,0,0), start, adj_end, width=1)
                     start = end
 
-            pygame.draw.circle(screen, (255, 255, 255),
+            pygame.draw.circle(self.screen, (255,255,255),
                                (self.wacom_x, self.wacom_y), 8)
-
             pygame.display.flip()
 
             current_time = time.time()
             duration = current_time - last_time
             remaining_time = 0.02 - duration
             if remaining_time > 0:
-                time.sleep(remaining_time)
+                await asyncio.sleep(remaining_time)
             last_time = current_time
 
-
 async def main():
+    parser = ArgumentParser()
+    parser.add_argument("-f", "--frontend", default="tcp://127.0.0.1:5559")
+    parser.add_argument("-b", "--backend", default="tcp://127.0.0.1:5560")
+    parser.add_argument("-p", "--patient", required=True, type=str)
+    parser.add_argument("-t", "--topic", default="Therapy")
+    args = parser.parse_args()
+
     pygame.init()
 
-    sub = Sub(TOPIC, "tcp://127.0.0.1:5559")
-    board = Board(TOPIC, "tcp://127.0.0.1:5560")
+    appt = Appointment(args.patient, args.topic, args.frontend, args.backend)
 
     await asyncio.gather(
-        sub.subscribe(),
-        board.recv_queue(),
-        board.draw(),
+        appt.subscribe(),
+        appt.draw(),
     )
 
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
